91ab81efe4c35c2c2f1fc65fbbba012c5a04c2dd
N1lan777 <muradilez228228228@gmail.com>
2026-07-09 20:22:59 +0300

    Stage 1: define GameState, Ball, Paddle, Block structures

--- include/game.h
+++ include/game.h
@@ -1,2 +1,39 @@
 #pragma once
 #include <stdbool.h>
+
+#define MAX_BLOCKS 50
+
+typedef struct {
+    float x;
+    float y;
+    float vx;
+    float vy;
+} Ball;
+
+typedef struct {
+    float x;
+    float y;
+    float width;
+    float height;
+} Paddle;
+
+typedef struct {
+    float x;
+    float y;
+    float width;
+    float height;
+    short hp;
+} Block;
+
+typedef struct {
+    Ball ball;
+    Paddle paddle;
+    bool is_running;
+    short score;
+    Block blocks[MAX_BLOCKS];
+} GameState;
+
+void game_init (GameState* state);
+void game_update (GameState* state, float dt);
+
+
