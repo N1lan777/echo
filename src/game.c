bce9f062bda053bdd4fa3de4d91dac0961d83090
N1lan777 <muradilez228228228@gmail.com>
2026-07-09 22:42:52 +0300

    stage 1 : game_init ( v1 )

---
+++ src/game.c
@@ -0,0 +1,15 @@
+#include "game.h"
+ void game_init(GameState* state){
+    state -> score = 0;
+    state -> is_running = true;
+    
+    state -> ball.x = 400;
+    state -> ball.y = 300;
+    state -> ball.vx = 50;
+    state -> ball.vy = 50;
+
+    state -> paddle.x = 375;
+    state -> paddle.y = 550;
+    state -> paddle.width = 50;
+    state -> paddle.height = 25;
+ }
