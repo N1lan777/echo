0fa52fdee9cc73eaeef27214a6bf5f918ed36ff2
Alegen-f <ilez.ts@proton.me>
2026-07-09 22:53:22 +0300

    Changed main file

--- src/main.c
+++ src/main.c
@@ -1,10 +1,7 @@
 #include <SDL.h>
+#include "../include/sdl_loop.h"
 
-int main(int argc, char* argv[]) {
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* win = SDL_CreateWindow("echo", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
-    SDL_Delay(2000);
-    SDL_DestroyWindow(win);
-    SDL_Quit();
+int main(void) {
+    sdl_loop();
     return 0;
 }
