8ad25f6dc559f3f6e251720c6c7fe356d35d2776
N1lan777 <muradilez228228228@gmail.com>
2026-07-10 21:36:05 +0300

    Stage 1: implement game_init - ball, paddle, and block grid initialization

--- src/game.c
+++ src/game.c
@@ -13,4 +13,14 @@
     state -> paddle.y = 550;
     state -> paddle.width = 50;
     state -> paddle.height = 25;
+
+    for (int i = 0; i < MAX_BLOCKS; ++i){
+      int col = i % 10;
+      int row = i / 10;
+      state -> blocks[i].x = 10 + col * 75;
+      state -> blocks[i].y = 50 + row * (25 + 10) ;
+      state -> blocks[i].width = 75;
+      state -> blocks[i].height = 25;
+      state -> blocks[i].hp = 1;
+    }
  }
