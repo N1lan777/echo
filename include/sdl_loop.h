89068e62b81a5cd7c17b84a67181e183f3b33cd4
Alegen-f <ilez.ts@proton.me>
2026-07-18 13:26:38 +0300

    test commit

--- include/sdl_loop.h
+++ include/sdl_loop.h
@@ -1,13 +1,8 @@
 #pragma once
 
-
 #include <SDL.h>
 #include "game.h"
+#include "vars.h"
 
-
-#define GAME_TITLE "echo"
-#define WIN_X 800
-#define WIN_Y 600
-
-
+void draw_circle(SDL_Renderer* renderer, int centerX, int centerY, int radius);
 void render(GameState* state, SDL_Renderer* renderer, const SDL_Color blocks_color[]);
