865e99a4477f3d33a9a9ddd262c23754af4d7612
Alegen-f <ilez.ts@proton.me>
2026-08-11 21:20:01 +0300

    katayus on volge

--- src/game.c
+++ src/game.c
@@ -266,17 +266,18 @@ void game_loading(Leaderboard* board)
 void game_leaderboard(Leaderboard* board, const char * name, int score)
 {
     if (board->count_scores < MAX_SCORES) {
-        for (int i = 0; i< board->count_scores; ++i)
-            if (
-                strcmp(board->scores[i].name,name) == 0
-                && board->scores[i].score < score
-            ) {
-                board->scores[i].score = score;
-                return;
+        for (int i = 0; i< board->count_scores; ++i) {
+            if (strcmp(board->scores[i].name,name) == 0) {
+                if (board->scores[i].score < score) {
+                    board->scores[i].score = score;
+                    continue;
+                }
+                else continue;
             }
 
-        strcpy(board->scores[board->count_scores].name,name);
-        board->scores[board->count_scores++].score = score;
+            strcpy(board->scores[board->count_scores].name,name);
+            board->scores[board->count_scores++].score = score;
+        }
     }
 }
 
